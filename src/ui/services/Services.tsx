86abb713e8aba3d595b9b6b9ced2de34107e82e7
Drew Kullman <agkullman@protonmail.com>
2024-04-21 23:43:34 -0800

    lint test 2

--- src/ui/services/Services.tsx
+++ src/ui/services/Services.tsx
@@ -2,17 +2,28 @@
 
 import React from 'react'
 import styles from './component.module.css'
-import { Cormorant } from 'next/font/google';
-
-const cormorant = Cormorant({
-    weight: '400',
-    subsets: ["latin"],
-    variable: '--font-cormorant'
-})
 
 export default function Services() {
   return (
-    <section className={`${cormorant.variable} ${styles.services}`}>
+    <section className={`${styles['services-grid']}`}>
+        <header className={`${styles['services-header']}`}>
+            <p className={`${styles['services-header-content-text']}`}>
+            Connecting Ties offers services and advocates for the disabled and elderly within the Valdez area and Copper River Basin.
+            </p>
+        </header>
+        <div className={`${styles['service']}`}>
+            <h1 className={`${styles['service-header']}`}>SERVICE</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nemo modi, cupiditate nesciunt obcaecati natus laborum illo voluptate tenetur unde? Accusantium dolorum, officiis quia cum saepe veniam ea laudantium nemo.</p>
+        </div>
+        <div className={`${styles['service']}`}>
+            <h1 className={`${styles['service-header']}`}>SERVICE</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nemo modi, cupiditate nesciunt obcaecati natus laborum illo voluptate tenetur unde? Accusantium dolorum, officiis quia cum saepe veniam ea laudantium nemo.</p>
+        </div>
+        <div className={`${styles['service']}`}>
+            <h1 className={`${styles['service-header']}`}>SERVICE</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nemo modi, cupiditate nesciunt obcaecati natus laborum illo voluptate tenetur unde? Accusantium dolorum, officiis quia cum saepe veniam ea laudantium nemo.</p>
+        </div>
+        
     </section>
   )
 }
