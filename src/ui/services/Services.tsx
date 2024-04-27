51310f307708b0a7cdf7d1fccb5cc875348d2cb3
Drew Kullman <agkullman@protonmail.com>
2024-04-26 18:25:25 -0800

    added ul to services-card

--- src/ui/services/Services.tsx
+++ src/ui/services/Services.tsx
@@ -13,41 +13,112 @@ const cormorant = Cormorant({
 export default function Services() {
   return (
     <section className="services-grid">
+
       <div className="service-card">
-        <h1>Service</h1>
+        <h1 className="service-title">Service</h1>
          <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste saepe odio temporibus fugit laborum minus, minima quisquam, enim perferendis magnam ducimus sed? Quas ab, ducimus assumenda placeat quasi provident sunt!
          </p>
+            <ul className="service-list">
+              <li>Service details here</li>
+              <li>Service details here</li>
+              <li>Service details here</li>
+            </ul>
+      </div>
+     
+      <div className="service-card">
+        <h1 className="service-title">Service</h1>
+         <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste saepe odio temporibus fugit laborum minus, minima quisquam, enim perferendis magnam ducimus sed? Quas ab, ducimus assumenda placeat quasi provident sunt!
+         </p>
+            <ul className="service-list">
+              <li>Service details here</li>
+              <li>Service details here</li>
+              <li>Service details here</li>
+            </ul>
       </div>
       <div className="service-card">
-        <h1>Service</h1>
+        <h1 className="service-title">Service</h1>
          <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste saepe odio temporibus fugit laborum minus, minima quisquam, enim perferendis magnam ducimus sed? Quas ab, ducimus assumenda placeat quasi provident sunt!
          </p>
+            <ul className="service-list">
+              <li>Service details here</li>
+              <li>Service details here</li>
+              <li>Service details here</li>
+            </ul>
       </div>
+      
       <div className="service-card">
-        <h1>Service</h1>
+        <h1 className="service-title">Service</h1>
          <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste saepe odio temporibus fugit laborum minus, minima quisquam, enim perferendis magnam ducimus sed? Quas ab, ducimus assumenda placeat quasi provident sunt!
          </p>
+            <ul className="service-list">
+              <li>Service details here</li>
+              <li>Service details here</li>
+              <li>Service details here</li>
+            </ul>
       </div>
+
       <div className="service-card">
-        <h1>Service</h1>
+        <h1 className="service-title">Service</h1>
          <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste saepe odio temporibus fugit laborum minus, minima quisquam, enim perferendis magnam ducimus sed? Quas ab, ducimus assumenda placeat quasi provident sunt!
          </p>
+            <ul className="service-list">
+              <li>Service details here</li>
+              <li>Service details here</li>
+              <li>Service details here</li>
+            </ul>
       </div>
+
       <div className="service-card">
-        <h1>Service</h1>
+        <h1 className="service-title">Service</h1>
          <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste saepe odio temporibus fugit laborum minus, minima quisquam, enim perferendis magnam ducimus sed? Quas ab, ducimus assumenda placeat quasi provident sunt!
          </p>
+            <ul className="service-list">
+              <li>Service details here</li>
+              <li>Service details here</li>
+              <li>Service details here</li>
+            </ul>
       </div>
+
       <div className="service-card">
-        <h1>Service</h1>
+        <h1 className="service-title">Service</h1>
          <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste saepe odio temporibus fugit laborum minus, minima quisquam, enim perferendis magnam ducimus sed? Quas ab, ducimus assumenda placeat quasi provident sunt!
          </p>
+            <ul className="service-list">
+              <li>Service details here</li>
+              <li>Service details here</li>
+              <li>Service details here</li>
+            </ul>
+      </div>
+
+      <div className="service-card">
+        <h1 className="service-title">Service</h1>
+         <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste saepe odio temporibus fugit laborum minus, minima quisquam, enim perferendis magnam ducimus sed? Quas ab, ducimus assumenda placeat quasi provident sunt!
+         </p>
+            <ul className="service-list">
+              <li>Service details here</li>
+              <li>Service details here</li>
+              <li>Service details here</li>
+            </ul>
+      </div>
+
+      <div className="service-card">
+        <h1 className="service-title">Service</h1>
+         <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste saepe odio temporibus fugit laborum minus, minima quisquam, enim perferendis magnam ducimus sed? Quas ab, ducimus assumenda placeat quasi provident sunt!
+         </p>
+            <ul className="service-list">
+              <li>Service details here</li>
+              <li>Service details here</li>
+              <li>Service details here</li>
+            </ul>
       </div>
       
     </section>
